ba55f48bacc778584f9986a924d17034f398d5f5
Cryptonoid2030 <137273973+Cryptonoid2030@users.noreply.github.com>
2025-01-15 21:52:04 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,6 @@ const ADDR: u8 = 0x18;
 pub fn get_ids(bus: &mut impl I2c) -> [u8; 2] {
     let mut buf = [0_u8; 1];
 
-    let res = bus.write_read(ADDR, &[0x00], &mut buf);
-
-    if res.is_err() {
-        log::error!("{:?}", res);
-    }
-
+    let res = bus.write_read(ADDR, &[0x00], &mut buf).unwrap();
     [buf[0], 0]
 }
